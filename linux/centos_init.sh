4cad6f761a9d8910d5822af1e8c7db30bdb78c2f
leemenix <leemenix@protonmail.com>
2020-12-24 11:37:58 +0100

    install ansible with python pip on centos

--- linux/centos_init.sh
+++ linux/centos_init.sh
@@ -7,5 +7,5 @@ fi
 yum update -y
 # ansible not part of inital repo; do it other way ;) 
 # 
-yum install git python2 -y
-pip install ansible -y 
+yum install git python3 python3-pip -y
+pip3 install ansible -y 
