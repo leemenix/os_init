74df7e344da0fb886c487ccb398a2445f324df2c
leemenix <leemenix@protonmail.com>
2020-12-24 19:33:39 +0100

    install ansible via pip

--- linux/debian_init.sh
+++ linux/debian_init.sh
@@ -4,8 +4,8 @@ if [[ $UID != 0 ]]; then
   echo "Please execute script as root user."; 
 fi
 
-apt update -y
-apt install git ansible -y
+apt install git python3 python3-pip -y
+pip3 install ansible
 
 # fetch ansible playbook
 cd /tmp ; rm -rf os_init; git clone https://github.com/leemenix/os_init.git
