8d49bbf0212f7f016171d472d19a194d410fa1a7
leemenix <leemenix@protonmail.com>
2020-12-24 14:01:19 +0100

    add slackware init script; install sbopkg

--- linux/slackware_init.sh
+++ linux/slackware_init.sh
@@ -10,4 +10,10 @@ fi
 slackpkg update
 
 # get & install sbopkg
-wget -q -O - https://github.com/sbopkg/sbopkg/releases/download/0.38.1/sbopkg-0.38.1-noarch-1_wsr.tgz | installpkg
+wget -O /tmp/sbopkg.tgz https://github.com/sbopkg/sbopkg/releases/download/0.38.1/sbopkg-0.38.1-noarch-1_wsr.tgz
+installpkg /tmp/sbopkg.tgz
+
+sqg -p ansible
+#Build and install packages from a created queuefile (answer Q for question if sbopkg should use queue or package):
+
+sbopkg -k -i ansible
