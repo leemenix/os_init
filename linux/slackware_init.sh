d12532923af8ba4ba3a803a12f5020e7ad6f15dd
leemenix <leemenix@protonmail.com>
2020-12-24 14:18:45 +0100

    add slackware init script; install sbopkg

--- linux/slackware_init.sh
+++ linux/slackware_init.sh
@@ -13,6 +13,16 @@ slackpkg update
 wget -O /tmp/sbopkg.tgz https://github.com/sbopkg/sbopkg/releases/download/0.38.1/sbopkg-0.38.1-noarch-1_wsr.tgz
 installpkg /tmp/sbopkg.tgz
 
+# as this need to be before run sbopkg
+#The following directories do not exist:
+#Variable                   Assignment
+#--------                   ----------
+mkdir -p /var/lib/sbopkg/,SBo/,14.2
+mkdir -p /var/log/sbopkg
+mkdir -p /var/lib/sbopkg/queues
+mkdir -p /var/cache/sbopkg
+mkdir -p /tmp/SBo
+
 sqg -p ansible
 #Build and install packages from a created queuefile (answer Q for question if sbopkg should use queue or package):
 
