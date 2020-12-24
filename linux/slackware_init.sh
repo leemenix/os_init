e3f5ca2b4ce87648489196774446b1a22be6161a
leemenix <leemenix@protonmail.com>
2020-12-24 14:52:46 +0100

    add slackware init script; install sbopkg

--- linux/slackware_init.sh
+++ linux/slackware_init.sh
@@ -25,7 +25,23 @@ mkdir -p /tmp/SBo
 # sync with local repo
 sbopkg -r 
 
-sqg -p ansible
+sbopkg -B -k -i python3
+sbopkg -B -k -i MarkupSafe 
+sbopkg -B -k -i Jinja2 
+sbopkg -B -k -i enum34 
+sbopkg -B -k -i ipaddress 
+sbopkg -B -k -i pycparser 
+sbopkg -B -k -i cffi 
+sbopkg -B -k -i asn1crypto 
+sbopkg -B -k -i idna 
+sbopkg -B -k -i pyparsing
+sbopkg -B -k -i six 
+sbopkg -B -k -i packaging 
+sbopkg -B -k -i pyasn1 
+sbopkg -B -k -i cryptography 
+
+
+#sqg -p ansible
 #Build and install packages from a created queuefile (answer Q for question if sbopkg should use queue or package):
 
 sbopkg -B -k -i ansible
