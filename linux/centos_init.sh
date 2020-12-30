dc2ed34cdaff54e5134a2ec54c3f80c1e7f42c94
leemenix <leemenix@protonmail.com>
2020-12-30 18:20:47 +0100

    without docker install

--- linux/centos_init.sh
+++ linux/centos_init.sh
@@ -15,4 +15,9 @@ su - deploy -c "pip3 install --user ansible"
 # fetch ansible playbook for initial bootstrap 
 # do all in /tmp dir and remove os_init dir first in case it exist
 su - deploy -c "cd /tmp; rm -rf os_init; git clone https://github.com/leemenix/os_init.git"
+
+# if you want deploy docker, during inital phase enable next line
+#su - deploy -c "ansible-playbook /tmp/os_init/linux/ansible/main.yml -e '{"deploy_docker": "True"}'"
+# if you enable previous line, you should comment next one , until more elegant solution is here
+# this will be automated via vars stored in gitlab, for local dev env
 su - deploy -c "ansible-playbook /tmp/os_init/linux/ansible/main.yml"
